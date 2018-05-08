3601ae42ef431c10459974ad732e9e32aa1f163f
vedmakelf <matveychukivan.r@gmail.com>
2018-05-08 21:30:55 +0300

    edit

--- docs/js/classJob.js
+++ docs/js/classJob.js
@@ -1,13 +1,13 @@
-﻿var x = 1, y=1;
-while (y <= 100) {
-    y++;
-    x = x + y;
-}
-console.log(`x=${x}`);
+﻿//var x = 1, y=1;
+//while (y <= 100) {
+//    y++;
+//    x = x + y;
+//}
+//console.log(`x=${x}`);
 
-var x = 1, y = 1;
-while (x <= 100) {
-    x++;
-    y += x;
-}
-console.log(`y=${y}`);
+//var x = 1, y = 1;
+//while (x <= 100) {
+//    x++;
+//    y += x;
+//}
+//console.log(`y=${y}`);
