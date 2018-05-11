b502dac7373102ed71b679d2f0fbe27c3fad506a
vedmakelf <matveychukivan.r@gmail.com>
2018-05-11 08:29:42 +0300

    edit

--- docs/js/classJob.js
+++ docs/js/classJob.js
@@ -11,3 +11,24 @@
 //    y += x;
 //}
 //console.log(`y=${y}`);
+
+
+//var obj1 = {
+//    name: `test`
+//};
+
+////function copy(obj) {
+////    obj.name = `test2`
+////    return obj;
+////}
+
+//var obj2 = obj1;
+//obj2.name = `test2`;
+
+//console.log(obj1.name);
+//console.log(obj2.name);
+
+var test = {
+    x: 0,
+    y: function () { console.log(test.x)}
+};
