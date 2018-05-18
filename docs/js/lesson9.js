a6096735bf77c53b476cd3eaa60562255e0d6497
vedmakelf <matveychukivan.r@gmail.com>
2018-05-18 08:41:53 +0300

    заготовка к заданию 9 урока

---
+++ docs/js/lesson9.js
@@ -0,0 +1,12 @@
+﻿function lesson9(task) {
+    switch (task) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break
+        case 4:
+            break;
+    }
+}
