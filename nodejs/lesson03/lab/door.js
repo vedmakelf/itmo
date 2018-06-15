e314a2e7501216202a19d04c9968d4e3f79da860
vedmakelf <matveychukivan.r@gmail.com>
2018-06-15 17:05:57 +0300

    правка

--- nodejs/lesson03/lab/door.js
+++ nodejs/lesson03/lab/door.js
@@ -3,7 +3,7 @@ var url = require("url");
 
 http.createServer((request, response) => {
     var data = url.parse(request.url, true).query;
-    process.send(pathname);
+    process.send(data);
     process.kill(process.pid);
 }).listen(8080, () => {
     console.log('start server');
